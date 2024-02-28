a618e5bf08c404c3930b60490e5036d6b9277470
Hollow678 <139318545+Hollow678@users.noreply.github.com>
2024-02-28 17:59:41 -0400

    Add files via upload

---
+++ Capitulo14.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package capitulo14;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Aluno
+ */
+public class Capitulo14 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        TextFieldFrame textFieldFrame = new TextFieldFrame();
+        textFieldFrame.setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
+        textFieldFrame.setSize(350,100);
+        textFieldFrame.setVisible(true);
+    }
+    
+}
